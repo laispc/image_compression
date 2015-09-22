513ac2950975b2495c90484e08456d0de02981f8
laispc <laispc19@gmail.com>
2015-09-21 23:25:04 -0300

    Dlist e mario imagens

---
+++ Dlist.h
@@ -0,0 +1,12 @@
+#ifndef DLIST__V10
+#define DLIST__V10
+
+/*
+Estrutura de dado que guarda o valor obtido no algorimo de diferença e o número de bits que esse  valor ocupa. 
+*/
+typedef struct Dlist_{
+	unsigned char value;
+	unsigned char numBits;
+}Dlist_;
+
+#endif
